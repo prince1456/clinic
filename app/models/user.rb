11a7cbfa1ea03a70b6c359ba51e49f4f6d0b9990
Ali Akbar Alizada <akbar.alizadeh5@gmail.com>
2017-05-17 14:00:27 -0700

    latest master branch

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_one :profile, dependent: :destroy
+  has_many :posts, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :liked_post, through: :likes, source: :post
+  has_many :comments, dependent: :destroy
+  has_many :messages, dependent: :destroy
+  has_many :subscriptions, dependent: :destroy
 
-  
+
 end
