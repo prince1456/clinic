d8f827037262766c1b0d440f8215ec67df6b7955
Ali Akbar Alizada <akbar.alizadeh5@gmail.com>
2017-05-27 17:30:58 -0700

    can can can

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,19 +7,22 @@ class Ability
     can :manage, Post do |post|
       post.user == user
     end
+    can :manage, Post do
+       User.where(role: "admin")
+    end
 
     # can :manage, Comment do |comment|
     #   comment.user == user
     # end
     # Define abilities for the passed in user here. For example:
-    #
+    # #
     #   user ||= User.new # guest user (not logged in)
-      # if user_admin?
-      #   can :manage, :all
-      # else
-      #   can :read, :all
-      # end
-    #
+    #   if user_admin?
+    #     can :manage, :all
+    #   else
+    #     can :read, :all
+    #   end
+    # # #
     # The first argument to `can` is the action you are giving the user
     # permission to do.
     # If you pass :manage it will apply to every action. Other common actions
