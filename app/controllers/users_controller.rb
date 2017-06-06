890d02f530abed46cb2e0b0c85f11bd74613c143
salman90 <salmansalem9022@gmail.com>
2017-06-06 13:27:32 -0700

    fixed devise

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  # def new
+  # end
+  #
+  # def create
+  #   @user = User.new user_params
+  #   if @user.save
+  #     session[:user_id] = @user
+  #     redirect_to dashboard_path
+  #   else
+  #     render new_user_registration_path
+  #   end
+  # end
+  # private
+  # def user_params
+  #   params.require(:user).permit(:email, :password, :password_confirmation)
+  # end
+end
