e7887bebc2666e786507495abf1005f401dadd0f
salman90 <salmansalem9022@gmail.com>
2017-06-08 16:43:09 -0700

    fixed image and added translation

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,6 @@ module Clinic
       config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # -- all .rb files in that directory are automatically loaded. 
   end
 end
