d14a92e0dee9109912ff312beda5c2f58786eca5
salman90 <salmansalem9022@gmail.com>
2017-06-28 15:34:40 -0700

    added validation and fixed comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+  validates :content, presence: {message: 'body has to be present'}
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,4 +2,15 @@ class Profile < ApplicationRecord
   has_many :directories, dependent: :destroy
   belongs_to :user
   mount_uploader :image, ImageUploader
+  validates :first_name,  presence: {message: "first name has to be present"}
+  validates :last_name,  presence: {message: "last name has to be present"}
+  validates :address, presence: {message: "address has to be present"}
+  validates :phone_number, presence: {message: "please add you phone number"}
+  validates :gender, presence: {message: "gender has to be present"}
+  validates :age, presence: {message: "age has to be present "}
+  validates :nationality, presence: {message: "nationality has to be present"}
+  validates :job, presence: {message: "Marital Status has to be present"}
+  validates :emr_name, presence: {message: " Emergency Contact Name has to be present"}
+  validates :emr_phone, presence: {message: "Emergency Contact Number has to be present"}
+  validates :lang, presence: {message: "language e has to be present"}
 end
